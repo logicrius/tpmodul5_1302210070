f185b20f5fa832cd76dc362c9353945cd4d1654d
logicrius <113976617+logicrius@users.noreply.github.com>
2023-03-19 21:09:33 +0700

    menambahkan class HaloGeneric dengan method SapaUser
    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_1302210070/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302210070
+{
+    internal class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("Halo User, " + x);
+        }
+    }
+}
--- tpmodul5_1302210070/Program.cs
+++ tpmodul5_1302210070/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul5_1302210070;
+
+class MainProgram
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric<String> menyapa = new HaloGeneric<String> { };
+        String x = "Ashar Syahbudi S";
+        menyapa.SapaUser(x);
+
+    }
+}
